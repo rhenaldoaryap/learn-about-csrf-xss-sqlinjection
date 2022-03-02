bb498c9fe3a08fe1079d747641631f3f145f7bd3
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-02 10:19:31 +0700

    install the xss package for sanitize the user-input

--- xss/routes/discussion.js
+++ xss/routes/discussion.js
@@ -1,26 +1,30 @@
-const express = require('express');
+const express = require("express");
+// import the xss package
+const xss = require("xss");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.redirect('/discussion');
+router.get("/", function (req, res) {
+  res.redirect("/discussion");
 });
 
-router.get('/discussion', async function (req, res) {
-  const comments = await db.getDb().collection('comments').find().toArray();
-  res.render('discussion', { comments: comments });
+router.get("/discussion", async function (req, res) {
+  const comments = await db.getDb().collection("comments").find().toArray();
+  res.render("discussion", { comments: comments });
 });
 
-router.post('/discussion/comment', async function (req, res) {
+router.post("/discussion/comment", async function (req, res) {
   const comment = {
-    text: req.body.comment,
+    // apply the xss package for prevent the XSS attacks.
+    // the xss package will sanitize and clean it and return the clean value.
+    text: xss(req.body.comment),
   };
 
-  await db.getDb().collection('comments').insertOne(comment);
+  await db.getDb().collection("comments").insertOne(comment);
 
-  res.redirect('/discussion');
+  res.redirect("/discussion");
 });
 
 module.exports = router;
