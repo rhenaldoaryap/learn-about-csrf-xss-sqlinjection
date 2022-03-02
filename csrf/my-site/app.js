ce4480657e62b61c2ff66770075dc04a7e8bd0bd
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-02 09:18:05 +0700

    installing csruf package for prevent CSRF attacks

--- csrf/my-site/app.js
+++ csrf/my-site/app.js
@@ -1,39 +1,50 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const session = require('express-session');
-const mongodbStore = require('connect-mongodb-session');
+const express = require("express");
+const session = require("express-session");
+const mongodbStore = require("connect-mongodb-session");
+// import csurf for preventing CSRF attacks
+const csrf = require("csurf");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
 
 const MongoDBStore = mongodbStore(session);
 
 const app = express();
 
 const sessionStore = new MongoDBStore({
-  uri: 'mongodb://localhost:27017',
-  databaseName: 'auth-demo',
-  collection: 'sessions'
+  uri: "mongodb://localhost:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  secret: 'super-secret',
-  resave: false,
-  saveUninitialized: false,
-  store: sessionStore,
-  cookie: {
-    maxAge: 2 * 24 * 60 * 60 * 1000
-  }
-}));
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+      // force default setting sameSite to "lax" for half-defense CSRF attack
+      sameSite: "lax",
+    },
+  })
+);
 
-app.use(async function(req, res, next) {
+// activating the csurf.
+// the default setting should do the trick for preventing the CSRF attacks, so no need special settings.
+// with that we can register the csrf setting at our routes.
+app.use(csrf());
+
+app.use(async function (req, res, next) {
   const user = req.session.user;
   const isAuth = req.session.isAuthenticated;
 
@@ -41,7 +52,10 @@ app.use(async function(req, res, next) {
     return next();
   }
 
-  const userDoc = await db.getDb().collection('users').findOne({_id: user.id});
+  const userDoc = await db
+    .getDb()
+    .collection("users")
+    .findOne({ _id: user.id });
   const isAdmin = userDoc.isAdmin;
 
   res.locals.isAuth = isAuth;
@@ -53,9 +67,9 @@ app.use(async function(req, res, next) {
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
